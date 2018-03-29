3cd9f91a16468dc47f5c69a52ba91ff2d59b2653
farmercode <wangchangchun120@gmail.com>
2018-03-29 10:23:19 +0800

    更新类的命名空间

--- src/WxShare.php
+++ src/WxShare.php
@@ -6,7 +6,7 @@
  * Time: 下午10:35
  */
 
-namespace FCode;
+namespace FCode\WxShare;
 
 use GuzzleHttp\Client;
 
--- test.php
+++ test.php
@@ -10,7 +10,7 @@ require "vendor/autoload.php";
 
 $appId = "wx2be37a2e643c5daa";
 $appSecret = "23a10fb3c44d6039c796df600528dcdf";
-$wxShare = new \FCode\WxShare($appId, $appSecret);
+$wxShare = new \FCode\WxShare\WxShare($appId, $appSecret);
 $url = "http://ab.aaa.com/";
 $result = $wxShare->getSign($url);
 print_r($result);
